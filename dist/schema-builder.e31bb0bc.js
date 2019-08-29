5fdcbcf01c9e6a3362f93cc1f208721b79cde5c7
Ras Brits <rasbrits@yahoo.com>
2019-08-29 15:13:40 +0200

    Nothing really changrd

--- dist/schema-builder.e31bb0bc.js
+++ dist/schema-builder.e31bb0bc.js
@@ -39863,7 +39863,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55151" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53609" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
